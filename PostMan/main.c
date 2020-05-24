df1022b89d5a032b9feb4c005b19363be65ab54b
unknown <jakub.marincic@gmail.com>
2020-05-24 11:39:31 +0200

    Done. - submit

--- PostMan/main.c
+++ PostMan/main.c
@@ -17,7 +17,6 @@ int getEarliestDepartureTime(int postmanId, int cityFrom, int currentCityTime) {
 	int earliestDepartureTime = 999999999;
 	int departureTime;
 
-	// TODO: Test this
 	// get the nearest departure time
 	for (int departureTimeId = 1; departureTimeId <= numberOfDepartures; departureTimeId++) {
 		departureTime = postman[postmanId][departureTimeId];
@@ -36,13 +35,6 @@ int getEarliestDepartureTime(int postmanId, int cityFrom, int currentCityTime) {
 	earliestDepartureTime += (currentCityTime / 100) * 100;
 
 	return earliestDepartureTime;
-}
-
-int calculateNextHopTime(int currentCityTime, int earliestDepartureTime) {
-	int nextHopTime;
-	
-	if (currentCityTime )
-	currentCityTime + earliestDepartureTime;
 }
 
 int notAllCitiesVisited(int citiesCount) {
@@ -87,6 +79,7 @@ int makeMove() {
 	nextStep[DEPARTURE_TIME] = nextStepList[bestMoveIndex][DEPARTURE_TIME];
 
 	currentTimes[nextStep[CITY_TO]] = nextStep[DEPARTURE_TIME];
+	printf("%d\n", currentTimes[nextStep[CITY_TO]]);
 
 	return nextStep[CITY_TO];
 }
@@ -99,8 +92,8 @@ void makeItRain(int allCitiesCount) {
 	while (notAllCitiesVisited(allCitiesCount)) {
 		currentCityTime = currentTimes[cityFrom];
 
-		// fill the tables for all routes from currentCity		// TODO: BUG go trought all the cities except ONE you came from
-		for (cityTo = 2; cityTo <= allCitiesCount; cityTo++) 
+		// fill the tables for all routes from currentCity
+		for (cityTo = 1; cityTo <= allCitiesCount; cityTo++) 
 		{
 			postmanId = citiesRelationsMatrix[cityFrom][cityTo];
 
@@ -108,15 +101,17 @@ void makeItRain(int allCitiesCount) {
 			if (postmanId == -1)
 				continue;
 
+			// check if the city was already visited
+			if (currentTimes[cityTo] != 999999999)
+				continue;
+
 			earliestDepartureTime = getEarliestDepartureTime(postmanId, cityFrom, currentCityTime);
 
 			insertIntoList(cityFrom, cityTo, earliestDepartureTime);
 		}
 
-		// pick best option
+		// pick best option -> move to next city
 		cityFrom = makeMove();
-
-		// cityFrom++;
 	}
 }
 
@@ -179,7 +174,7 @@ int main() {
 		postman = (int**)malloc((postmanCount + 2) * sizeof(int*));
 
 		currentTimes = (int*)malloc((citiesCount + 2) * sizeof(int));
-		for (int cityTime = 0; cityTime < citiesCount + 1; cityTime++) {				// plus one, so you can ignore 0 row and column	
+		for (int cityTime = 0; cityTime < citiesCount + 1; cityTime++) {				// plus one, so you can ignore 0 row and column	=> city 1 has index 1
 			currentTimes[cityTime] = 999999999;
 		}
 		currentTimes[0] = -99;
@@ -208,7 +203,6 @@ int main() {
 
 			// init and load postman time-schedule
 			// printf("Postmanid: %d -- numberOfDepartureTimes: %d\n", postmanId, numberOfDepartureTimes);
-			int kokot = numberOfDepartureTimes;
 			postman[postmanId] = (int*)malloc((numberOfDepartureTimes + 1) * sizeof(int));
 			postman[postmanId][0] = numberOfDepartureTimes;
 			loadDepartureTimes(postmanId, numberOfDepartureTimes);
@@ -235,8 +229,6 @@ int main() {
 		}
 		free(nextStepList);
 	}
-	// printf("done");
-
 
 	return 0;
 }
